f42d23593e8a13fbe1a139b2625dd57febfee84f
Mark Eliasen <mark.eliasen@zolidsolutions.com>
2018-11-17 00:22:41 +0000

    initial commit

---
+++ src/components/commands.js
@@ -0,0 +1,91 @@
+import {RichEmbed} from 'discord.js';
+
+class Commands {
+    constructor() {
+    }
+
+    /**
+     * Formats a number to 2 decimal points
+     * @param  {Number} number The number to format
+     * @return {Number}
+     */
+    formatNumberDecimal(number) {
+        return Math.max(0, Math.round(number * 100) / 100);
+    }
+
+    /**
+     * Generates a random value between min and max
+     * @param  {Number} min
+     * @param  {Number} max
+     * @return {Number}
+     */
+    between(min, max) {
+        min = parseInt(min, 10);
+        max = parseInt(max, 10);
+
+        if (isNaN(min) || isNaN(max)) {
+            return 0;
+        }
+
+        return Math.floor(
+            (Math.random() * (
+                Math.max(min, max) - Math.min(min, max)
+            )) + Math.min(min, max)
+        );
+    }
+
+    parse = (message) => {
+        // Dice roll
+        // /roll 2d4
+        if (message.content.indexOf('/roll') === 0) {
+            this.cmdDice(message);
+            return;
+        }
+    }
+
+    cmdDice(message) {
+        const errorMsg = 'The command is: `/roll xdy` or `/roll x d y`, where x is the number of dies to roll, and y being the type of die. Eg: `/roll 4d6`';
+        let rollString = message.content.trim().toLowerCase();
+
+        if (rollString === '/roll') {
+            message.reply(errorMsg);
+            return;
+        }
+
+        // remove the /roll prefix
+        rollString = rollString.replace('/roll ', '');
+        // remove the spaces as they are not needed
+        rollString = rollString.replace(/\s/, '');
+
+        // split the roll params
+        let params = rollString.split('d');
+
+        if (params.length !== 2) {
+            message.reply(errorMsg);
+            return;
+        }
+
+        let diceCount = parseInt(params[0], 10);
+        let diceType = parseInt(params[1], 10);
+
+        if (isNaN(diceCount) || isNaN(diceType)) {
+            message.reply('You must specify a valid and positive number for the number of dies and the type of die.');
+            return;
+        }
+
+        const result = this.formatNumberDecimal(diceCount * diceType);
+
+        const rollResults = new RichEmbed()
+            // Set the title of the field
+            .setTitle(`:game_die: Rolling ${diceCount}D${diceType}:`)
+            // Set the color of the embed
+            .setColor(0xFF0000)
+            // Set the main content of the embed
+            .setDescription(`Result: ${result}`);
+
+        // Send the embed to the same channel as the message
+        message.channel.send(rollResults);
+    }
+}
+
+export default Commands;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import Discord from 'discord.js';
+//import Logger from './components/logger';
+import Commands from './components/commands';
+
+// load .env file
+const dotloaded = dotenv.config();
+if (dotloaded.error) {
+    throw new Error(dotloaded.error);
+}
+
+// Create the bot
+const client = new Discord.Client();
+
+// load out commands
+const commands = new Commands();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', commands.parse);
+
+client.login(process.env.DOKKU_DISCORD_TOKEN || process.env.DISCORD_TOKEN);
+
+// To join the bot on a server, open in browser:
+// https://discordapp.com/oauth2/authorize?client_id=xxxxxx&scope=bot
